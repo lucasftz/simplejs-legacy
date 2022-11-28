fdcb2831d354f8edd8076d3985efcdabac3dfa27
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-27 21:38:17 -0600

    Remove now deleted server import

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,5 +1,4 @@
 import { h } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/src/dom/h/index.ts";
 import { render } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/src/dom/render/index.ts";
-import { server } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/src/dom/server/index.ts";
 
-export { h, render, server };
+export { h, render };
