fe95406ef4dd77db66c1f9d2dcc559b45f1e45bf
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-28 01:37:20 -0600

    Push render code

--- src/dom/render/index.ts
+++ src/dom/render/index.ts
@@ -1,5 +1,13 @@
-function render(component: () => HTMLElement, mount: HTMLElement) {
-  mount.appendChild(component());
+function createMount() {
+  const mount = document.createElement("div");
+  document.appendChild(mount);
+  return mount;
+}
+
+function render(component: () => HTMLElement, mount: HTMLElement | null) {
+  const _mount = mount ?? createMount();
+
+  _mount.appendChild(component());
 }
 
 export { render };
