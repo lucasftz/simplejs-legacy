c3c5a1940347f335257cd5a8c6a45a39cdac4808
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-28 08:54:07 -0600

    Add import map to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,7 @@
 import * as packup from "https://deno.land/x/packup@v0.2.2/cli.ts";
 
 function build() {
-  packup.main(["index.html"]);
+  packup.main(["index.html", "--import-map", "import_map.json"]);
 }
 
 function serve() {
