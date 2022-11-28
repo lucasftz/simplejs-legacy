877cdc9c4059238c0d9a20020e9cb3e22d6bfe96
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-28 13:12:56 -0600

    Add useEffect hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,38 @@
-import { useSignal } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/src/hooks/useSignal/index.ts";
+// deno-lint-ignore ban-types
+type Fn = Function;
 
-export { useSignal };
+const context: Fn[] = [];
+const getCurrentObserver = () => context[context.length - 1];
+
+function useSignal<T>(value: T): [() => T, (next: T) => void] {
+  const subscribers = new Set<Fn>();
+
+  const read = () => {
+    const current = getCurrentObserver();
+    if (current) subscribers.add(current);
+    return value;
+  };
+
+  const write = (next: T) => {
+    value = next;
+    subscribers.forEach((sub) => sub());
+  };
+
+  return [read, write];
+}
+
+function useEffect(fn: Fn) {
+  const execute = () => {
+    context.push(execute);
+
+    try {
+      fn();
+    } finally {
+      context.pop();
+    }
+  };
+
+  execute();
+}
+
+export { useSignal, useEffect };
