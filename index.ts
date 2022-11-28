ebff71ffb8dd9092504650caeb79418eb6673dee
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-27 21:37:24 -0600

    More bugfixing with imports

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import {
   h,
   render,
-  server,
 } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/src/dom/index.ts";
 
-export { h, render, server };
+export { h, render };
--- src/dom/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std@0.53.0/http/server.ts";
-
-const server = serve({ port: 3000 });
-
-console.log("Now serving on port 3000 🔥");
-
-for await (const req of server) {
-  req.respond({ body: "Hello World" });
-}
