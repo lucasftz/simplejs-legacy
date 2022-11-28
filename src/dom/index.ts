81924060207bcf8babf16317c5be8c7499eec1f1
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-27 21:31:35 -0600

    Update more urls for dom

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,5 +1,5 @@
-import { h } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/dom/h/index.ts";
-import { render } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/dom/render/index.ts";
-import { server } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/dom/server/index.ts";
+import { h } from "https://raw.githubusercontent.com/lucasftz/simplejs/main/src/dom/h/index.ts";
+import { render } from "https://raw.githubusercontent.com/lucasftz/simplejs/src/main/dom/render/index.ts";
+import { server } from "https://raw.githubusercontent.com/lucasftz/simplejs/src/main/dom/server/index.ts";
 
 export { h, render, server };
