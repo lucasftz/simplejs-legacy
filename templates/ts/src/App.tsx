4c15a64a31eccbaeaeac7c9e278c6395338075d2
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-11-28 18:28:12 -0600

    Add counter to template

--- templates/ts/src/App.tsx
+++ templates/ts/src/App.tsx
@@ -1,9 +1,15 @@
 import { h as _h } from "simplejs/dom";
+import { Counter } from "~/src/Counter.tsx";
 
 /* @jsx _h */
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h1 class="some-class">Hello world</h1>
+      <Counter />
+    </div>
+  );
 }
 
 export { App };
---
+++ templates/ts/src/Counter.tsx
@@ -0,0 +1,16 @@
+import { h as _h } from "simplejs/dom";
+import { useSignal } from "simplejs/hooks";
+
+/* @jsx _h */
+
+function Counter() {
+  const [count, setCount] = useSignal(0);
+
+  return (
+    <button onClick={() => setCount(count() + 1)}>
+      {() => "Count: " + count()}
+    </button>
+  );
+}
+
+export { Counter };
